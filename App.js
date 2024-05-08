b57ea332d65d2d5c590a71031908bc4b387b141d
Rashmi <rashmikinha2001@gmail.com>
2024-05-08 23:29:19 +0530

    .

---
+++ App.js
@@ -0,0 +1,5 @@
+// hello world in react
+const heading = React.createElement('h1',{id:"heading"},"Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+   
