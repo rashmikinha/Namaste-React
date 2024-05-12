ffb0d41d5666aec1685c0ef8a896ebf598b09e10
Rashmi <rashmikinha2001@gmail.com>
2024-05-12 23:11:57 +0530

    first

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 // hello world in react
 const heading = React.createElement('h1',{id:"heading"},"Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById('root'));
